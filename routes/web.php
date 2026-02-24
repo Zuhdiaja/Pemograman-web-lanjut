6d674e3e6303af28a197a919daa1a7176f98c0a3
Zuhdiaja <145771767+Zuhdiaja@users.noreply.github.com>
2026-02-24 22:15:43 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,37 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'selamat datang';
 });
+
+//praktikum 1
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'Muhammad Zuhdi Yudadharma <br>
+        NIM : 244107020017';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/article/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = 'Zuhdi') {
+    return 'Nama saya ' . $name;
+});
+
+//praktikum 2
